4eeb984692dffb77ead159fd6b8aaee46c0a645a
praveen <gontupraveenreddy@gmail.com>
2019-04-23 13:17:25 +0530

    math

---
+++ math/app/simple-math.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("This is a beginning of simple Math Module!");
+var advance_math_1 = require("./advance-math");
+function adition(num1, num2) {
+    return num1 + num2;
+}
+exports.adition = adition;
+function subtraction(num1, num2) {
+    return num1 - num2;
+}
+exports.subtraction = subtraction;
+function multiplication(num1, num2) {
+    return num1 * num2;
+}
+function division(num1, num2) {
+    return num1 / num2;
+}
+console.log(advance_math_1.square(10, 20));
+console.log("this is an eddding of simple math Module");
+//# sourceMappingURL=simple-math.js.map
